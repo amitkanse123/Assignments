8c061b19730cd0097f92c67b90e8ba83de60f693
amitkanse123 <you@example.com>
2021-02-10 09:34:59 +0530

    javascript introduction

---
+++ DAY21/javascript/a.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = true;
+c = a + b;
+
+a = "amit"
+console.log(typeof(c))
+console.log(typeof(b))
+console.log(a)
+var b = 10;
+var c = 1;
+while (c <= 10) {
+    console.log(c);
+    c++;
+}
+var k = 5;
+var fact = 1;
+while (k != 1) {
+    fact = fact * k;
+    k--;
+}
+console.log(fact);
